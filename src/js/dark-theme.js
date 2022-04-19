d22f89ccd8272920fd96b4001c2917da5b868f25
ruhend <54891285+ruhend@users.noreply.github.com>
2022-04-19 09:37:21 +0530

    updated design and added publication

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -3,16 +3,16 @@ function theme_switcher() {
     const cb = document.getElementById('mode-setter');
     console.log("Theme switcher");
     var rootElement = document.querySelector(':root')
-    if (!cb.checked) { // dark mode disabled
-        rootElement.style.setProperty('--backg-color', "#ffffff")
-        rootElement.style.setProperty('--backg-color2', "#222222")
-        rootElement.style.setProperty('--second-color', "#121212")
+    if (cb.checked) { // dark mode disabled
+        rootElement.style.setProperty('--backg-color', "#ebebeb")
+        rootElement.style.setProperty('--backg-color2', "#666666")
+        rootElement.style.setProperty('--second-color', "#666666")
         rootElement.style.setProperty('--nav-color', "#f2f2f2")
     }
-    if (cb.checked) { // dark mode enabled
-        rootElement.style.setProperty('--backg-color', "#222")
-        rootElement.style.setProperty('--backg-color2', "#ffffff")
-        rootElement.style.setProperty('--second-color', "#ffffff")
-        rootElement.style.setProperty('--nav-color', "#2c2c2c")
+    if (!cb.checked) { // dark mode enabled
+        rootElement.style.setProperty('--backg-color', "#666666")
+        rootElement.style.setProperty('--backg-color2', "#ebebeb")
+        rootElement.style.setProperty('--second-color', "#ebebeb")
+        rootElement.style.setProperty('--nav-color', "#666666")
     }
 }
